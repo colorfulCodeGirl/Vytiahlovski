397bc2b8d936f9be2b6490d646ac8e50e323806b
Vanilna <lesyalp@gmail.com>
2019-09-04 16:35:59 +0200

    added tabKey tappong function for open menu.

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 /*
-hiding hamburger menu from keybord users when menu is open.
+Traping tabKey incide open menu
 */
-
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-hamburgerMenu.addEventListener('click', () => hamburgerMenu.setAttribute('tabindex', -1));
+const closeMenu = document.querySelector('.close-menu');
+const menuTogglingElements = [hamburgerMenu, closeMenu];
+
+const trapTabKey = () => {
+    const menu = document.querySelector('nav');
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
+    const hideWhenMenuOpen = [main, footer];
+
+    if (menu.hidden) {
+        hideWhenMenuOpen.forEach((elem) => elem.hidden = true);
+        hamburgerMenu.setAttribute('tabindex', -1);
+        menu.hidden = false;
+    } else {
+        hideWhenMenuOpen.forEach((elem) => elem.hidden = false);
+        hamburgerMenu.setAttribute('tabindex', 0);
+        menu.hidden = true;
+    }
+};
+
+menuTogglingElements.forEach((elem) => elem.addEventListener('click', trapTabKey));
