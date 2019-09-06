8548a81da4e2dfe8208a45f137205dedefcb4a6f
Vanilna <lesyalp@gmail.com>
2019-09-06 11:30:00 +0200

    Fixed trapTabKey funktion. Animated menu sliding.

--- main.js
+++ main.js
@@ -2,24 +2,22 @@
 Traping tabKey incide open menu
 */
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-const closeMenu = document.querySelector('.close-menu');
-const menuTogglingElements = [hamburgerMenu, closeMenu];
+const menu = document.querySelector('nav');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+const hideWhenMenuOpen = [main, footer, hamburgerMenu];
+let menuTogglingElements = menu.querySelectorAll('a');
+menuTogglingElements = Array.prototype.slice.call(menuTogglingElements);
+menuTogglingElements.push(hamburgerMenu);
 
 const trapTabKey = () => {
-    const menu = document.querySelector('nav');
-    const main = document.querySelector('main');
-    const footer = document.querySelector('footer');
-    const hideWhenMenuOpen = [main, footer];
-
-    if (menu.hidden) {
-        hideWhenMenuOpen.forEach((elem) => elem.hidden = true);
-        hamburgerMenu.setAttribute('tabindex', -1);
-        menu.hidden = false;
+    if (menu.getAttribute('aria-hidden') == 'true') {
+        setTimeout(() => hideWhenMenuOpen.forEach(elem => elem.classList.add('hidden')), 1000);
+        menu.setAttribute('aria-hidden', 'false');
     } else {
-        hideWhenMenuOpen.forEach((elem) => elem.hidden = false);
-        hamburgerMenu.setAttribute('tabindex', 0);
-        menu.hidden = true;
+        hideWhenMenuOpen.forEach(elem => elem.classList.remove('hidden'));
+        menu.setAttribute('aria-hidden', 'true');
     }
 };
 
-menuTogglingElements.forEach((elem) => elem.addEventListener('click', trapTabKey));
+menuTogglingElements.forEach(elem => elem.addEventListener('click', trapTabKey));
