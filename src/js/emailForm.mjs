8bc03b4ffc137ee93c6ed4d0b19191bdb01c62a1
Vanilna <lesyalp@gmail.com>
2019-12-04 19:22:58 +0100

    changed class names in EmailForm.js due to BEM changes

--- src/js/emailForm.mjs
+++ src/js/emailForm.mjs
@@ -4,24 +4,25 @@ class EmailForm {
   }
 
   openMailForm() {
-    const btn = this.section.querySelector('.email-form-toggler');
-    const emailForm = this.section.querySelector('.email');
-    const overlay = this.section.querySelector('.form-overlay');
+    const btn = this.section.querySelector('.button--email-toggler');
+    const emailForm = this.section.querySelector('.email-form');
+    const overlay = this.section.querySelector('.overlay');
     const header = document.querySelector('.menu-toggler');
-    const closeBtn = emailForm.querySelector('.close');
-    const submitBtn = emailForm.querySelector('.submit');
+    const closeBtn = emailForm.querySelector('.button--close');
+    const submitBtn = emailForm.querySelector('.button--submit');
 
     const toggleForm = (e) => {
       const isMenu = e.target.className.includes('menu');
+      const isFormActive = emailForm.className.includes('active');
 
       /* don't open email form when clicking on menu toggler */
-      if (isMenu && !emailForm.className.includes('active')) return;
+      if (isMenu && !isFormActive) return;
 
-      emailForm.classList.toggle('active');
-      overlay.classList.toggle('active');
+      emailForm.classList.toggle('email-form--active');
+      overlay.classList.toggle('overlay--active');
     };
 
-    [btn, header, closeBtn, submitBtn].forEach((element) => {
+    [btn, header, closeBtn, submitBtn, overlay].forEach((element) => {
       element.addEventListener('click', toggleForm);
     });
   }
