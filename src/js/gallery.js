f25b9709d14f3daabd18e9bb24a74a765a8259cc
Vanilna <lesyalp@gmail.com>
2019-12-16 11:39:27 +0100

    made openFullImage function in gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -8,6 +8,7 @@ import EmailForm from './EmailForm';
 import '../css/main.css';
 import '../css/gallery.css';
 
+// eslint-disable-next-line import/extensions
 import data from '../assets/tapestry.json';
 
 class Gallery {
@@ -25,24 +26,24 @@ class Gallery {
   }
 
   populateWithImg() {
-    const imgTagList = [];
     for (let i = this.imageCount; i < this.imageCount + 10; i++) {
       const img = this.claud.imageTag(`${this.person}/${i}`, {
         transformation: ['gallery_prevue_desktop'],
         fetchFormat: 'auto',
       });
-      const htmlTag = img.toHtml();
-      const htmlBlock = `
-            <div class="image-block">
-                ${htmlTag}
-                <div class="image-block__popup">
+
+      const div = document.createElement('div');
+      div.innerHTML = `
+                ${img.toHtml()}
+                <div class="image-block__popup" data-index=${i}>
                   <p class="image-block__text">${this.data[i].name}</p>
                 </div>
-            </div>
       `;
-      imgTagList.push(htmlBlock);
+      div.classList.add('image-block');
+      div.setAttribute('data-index', i);
+      this.gallery.appendChild(div);
+      // div.addEventListener('click', () => this.openFullImage.bind(this));
     }
-    this.gallery.innerHTML = imgTagList.join('');
     this.imageCount = this.imageCount + 20;
   }
 
@@ -64,10 +65,45 @@ class Gallery {
     });
   }
 
+  openFullImage(e) {
+    const { index } = e.target.parentElement.dataset;
+    const fullImageBlock = document.querySelector('.full-image');
+    const overlay = fullImageBlock.querySelector('.overlay');
+    const info = fullImageBlock.querySelector('.full-image__info');
+    const height = window.innerHeight;
+    fullImageBlock.style.display = 'grid';
+    fullImageBlock.style.height = `${height}px`;
+    overlay.style.height = `${height}px`;
+
+    const image = new Image();
+    image.src = this.claud.url(`${this.person}/${index}`, {
+      height: height.toFixed(0),
+      quality: 'auto:good',
+      crop: 'scale',
+      fetchFormat: 'auto',
+    });
+
+    image.addEventListener('load', () => {
+      info.style.height = `${image.height}px`;
+      info.style.width = `${image.width + 250}px`;
+    });
+    overlay.classList.add('overlay--active');
+    image.classList.add('full-image__image');
+    fullImageBlock.insertBefore(image, info);
+
+    const text = `
+    <p class='full-image__text'>${this.data[index].name}</p>
+    <p class='full-image__text'>${this.data[index].material},</p>
+    <p class='full-image__text'>${this.data[index].size}, ${this.data[index].year}</p>
+    `;
+    info.innerHTML = text;
+  }
+
   init() {
     this.fetchData();
     this.populateWithImg();
     this.createMasonryLayout();
+    this.gallery.addEventListener('click', this.openFullImage.bind(this));
   }
 }
 
@@ -87,4 +123,5 @@ const options = {
   linkSelector: `a[href^="${window.location.origin}"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])`,
 };
 
+// eslint-disable-next-line no-unused-vars
 const swup = new Swup(options);
